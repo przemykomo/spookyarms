ed96ab79deba7403caa57cd6a7fa4b70116a360a
Przemek <przemek@przemyk.xyz>
2020-10-20 08:50:19 +0200

    upgrade kit tooltip

--- src/main/java/xyz/przemyk/spookyarms/registry/ItemsRegistry.java
+++ src/main/java/xyz/przemyk/spookyarms/registry/ItemsRegistry.java
@@ -1,6 +1,11 @@
 package xyz.przemyk.spookyarms.registry;
 
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.*;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -8,6 +13,8 @@ import net.minecraftforge.registries.ForgeRegistries;
 import xyz.przemyk.spookyarms.guns.PumpkinBazookaItem;
 import xyz.przemyk.spookyarms.guns.PumpkinPistolItem;
 import xyz.przemyk.spookyarms.SpookyArms;
+
+import java.util.List;
 
 @SuppressWarnings("unused")
 public class ItemsRegistry {
@@ -36,6 +43,13 @@ public class ItemsRegistry {
     public static final RegistryObject<Item> PUMPKIN_BULLET = ITEMS.register("pumpkin_bullet", () -> new Item(new Item.Properties().group(MOD_ITEM_GROUP)));
     public static final RegistryObject<Item> INFINITE_AMMO_POUCH = ITEMS.register("infinite_ammo_pouch", () -> new Item(new Item.Properties().group(MOD_ITEM_GROUP)));
     public static final RegistryObject<Item> PUMPKIN_CIRCUIT = ITEMS.register("pumpkin_circuit", () -> new Item(new Item.Properties().group(MOD_ITEM_GROUP)));
-    public static final RegistryObject<Item> UPGRADE_KIT = ITEMS.register("upgrade_kit", () -> new Item(new Item.Properties().group(MOD_ITEM_GROUP)));
+
+    private static final TranslationTextComponent TOOLTIP = new TranslationTextComponent(SpookyArms.MODID + ".pumpkin_circuit.info");
+    public static final RegistryObject<Item> UPGRADE_KIT = ITEMS.register("upgrade_kit", () -> new Item(new Item.Properties().group(MOD_ITEM_GROUP).rarity(Rarity.UNCOMMON)) {
+        @Override
+        public void addInformation(ItemStack stack, World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+            tooltip.add(TOOLTIP);
+        }
+    });
 
 }
